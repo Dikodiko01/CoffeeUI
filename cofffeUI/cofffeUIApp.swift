b73f2ba9e24b06d8ec32921c8cf2a1bd81367f64
dikodiko01 <dikodiko901123@gmail.com>
2022-10-22 20:01:21 +0600

    Initial Commit

---
+++ cofffeUI/cofffeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  cofffeUIApp.swift
+//  cofffeUI
+//
+//  Created by Diar Orynbek on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct cofffeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
